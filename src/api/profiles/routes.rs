98e7dea73464f6439cb6e6b8f961fac69bcde4cb
WookiesRpeople2 <117142278+WookiesRpeople2@users.noreply.github.com>
2026-06-11 15:25:24 +0200

    chore: fix create profile

--- src/api/profiles/routes.rs
+++ src/api/profiles/routes.rs
@@ -1,12 +1,10 @@
 use axum::{
-    routing::{delete, get, patch, post},
+    routing::{delete, get, patch},
     Router,
 };
 
 use crate::{
-    profiles::handlers::{
-        create_profile, delete_profile, get_profile, list_profiles, update_profile,
-    },
+    profiles::handlers::{delete_profile, get_profile, list_profiles, update_profile},
     AppState,
 };
 
